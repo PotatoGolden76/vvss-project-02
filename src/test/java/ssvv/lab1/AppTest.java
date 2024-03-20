5ba1cc6d18140138386b918e36fccc3a3f47f2c4
PotatoGolden76 <robert.harangus@gmail.com>
2024-03-20 08:52:27 +0200

    testare

--- src/test/java/ssvv/lab1/AppTest.java
+++ src/test/java/ssvv/lab1/AppTest.java
@@ -1,8 +1,16 @@
 package ssvv.lab1;
 
+import domain.Student;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
 
 /**
  * Unit test for simple App.
@@ -10,6 +18,18 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+    StudentValidator studentValidator = new StudentValidator();
+    TemaValidator temaValidator = new TemaValidator();
+    String filenameStudent = "fisiere/Studenti.xml";
+    String filenameTema = "fisiere/Teme.xml";
+    String filenameNota = "fisiere/Note.xml";
+
+    StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+    TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+    NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+    NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+    Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
     /**
      * Create the test case
      *
@@ -34,5 +54,27 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void testAddStudent1()
+    {
+        Student testStudent = new Student("test_id", "Test Student", 933, "test_email@stud.ubbcluj.ro");
+        try {
+            service.addStudent(testStudent);
+            assertTrue( true );
+        } catch (Exception e){
+            fail();
+        }
+    }
+
+    public void testAddStudent2()
+    {
+        Student testStudent = new Student("test_id", "Test Student", -933, "test_email@stud.ubbcluj.ro");
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (Exception e){
+            assertTrue( true );
+        }
     }
 }
