fe6aae4d61954765b2ad40c15b50c8152ca921b5
PotatoGolden76 <robert.harangus@gmail.com>
2024-03-28 13:28:17 +0200

    Update Teste

--- src/test/java/ssvv/lab1/AppTest.java
+++ src/test/java/ssvv/lab1/AppTest.java
@@ -11,6 +11,7 @@ import service.Service;
 import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
+import validation.ValidationException;
 
 /**
  * Unit test for simple App.
@@ -48,23 +49,16 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
     public void testAddStudent1()
     {
         Student testStudent = new Student("test_id", "Test Student", 933, "test_email@stud.ubbcluj.ro");
         try {
             service.addStudent(testStudent);
-            assertTrue( true );
         } catch (Exception e){
             fail();
         }
+        service.deleteStudent("test_id");
+        assertTrue( true );
     }
 
     public void testAddStudent2()
@@ -72,9 +66,142 @@ public class AppTest
         Student testStudent = new Student("test_id", "Test Student", -933, "test_email@stud.ubbcluj.ro");
         try {
             service.addStudent(testStudent);
+//            service.deleteStudent("test_id");
             fail();
         } catch (Exception e){
             assertTrue( true );
         }
+    }
+
+    public void testCase1()
+    {
+        Student testStudent = new Student("1", "John Doe", 933, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+        } catch (Exception e){
+            fail();
+        }
+        service.deleteStudent("1");
+        assertTrue( true );
+    }
+    public void testCase2()
+    {
+        Student testStudent = new Student("2", "John Doe", -9, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (ValidationException e) {
+            assertTrue(true);
+        }
+    }
+
+    public void testCase3()
+    {
+        Student testStudent = new Student("3", "John Doe", 0, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+        } catch (Exception e){
+            fail();
+        }
+        service.deleteStudent("3");
+        assertTrue( true );
+    }
+    public void testCase5()
+    {
+        Student testStudent = new Student("1", "", 933, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (ValidationException e){
+            assertTrue( true );
+        }
+    }
+    public void testCase6()
+    {
+        Student testStudent = new Student("1", null, 933, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (ValidationException e){
+            assertTrue( true );
+        }
+    }
+    public void testCase7()
+    {
+        Student testStudent = new Student("2345", "John Doe", 0, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+        } catch (Exception e){
+            fail();
+        }
+        service.deleteStudent("2345");
+        assertTrue( true );
+    }
+    public void testCase8()
+    {
+        Student testStudent = new Student("", "John Doe", 933, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (ValidationException e){
+            assertTrue( true );
+        }
+    }
+    public void testCase9()
+    {
+        Student testStudent = new Student(null, "John Doe", 933, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (ValidationException e){
+            assertTrue( true );
+        }
+    }
+    public void testCase11()
+    {
+        Student testStudent = new Student("2", "John Doe", 933, "");
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (ValidationException e){
+            assertTrue( true );
+        }
+    }
+    public void testCase12()
+    {
+        Student testStudent = new Student("2", "John Doe", 933, null);
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (ValidationException e){
+            assertTrue( true );
+        }
+    }
+    public void testCase13()
+    {
+        Student testStudent = new Student("1", "John Doe", 0, "johndoe@ubb.ro");
+        service.addStudent(testStudent);
+        try {
+            service.addStudent(testStudent);
+        } catch (Exception e){
+            fail();
+        }
+        service.deleteStudent("1");
+        assertTrue( true );
+    }
+    public void testCase14()
+    {
+        Student testStudent = new Student("1", "John Doe", 0, "johndoe@ubb.ro");
+        service.addStudent(testStudent);
+
+        testStudent = new Student("2", "John Doe", 0, "johndoe@ubb.ro");
+        try {
+            service.addStudent(testStudent);
+        } catch (Exception e){
+            fail();
+        }
+        service.deleteStudent("1");
+        service.deleteStudent("2");
+        assertTrue( true );
     }
 }
